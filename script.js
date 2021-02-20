6c1849b3ebad31a5ec2bd740fd52ee697df5784f
colejbodine <colejbodine@gmail.com>
2021-02-19 19:34:27 -0500

    Updated styles.

--- script.js
+++ script.js
@@ -5,8 +5,34 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const month = e.target.querySelector('#month').value;
     const day = e.target.querySelector('#day').value;
-    const dateUrl = "http://numbersapi.com/"+month+"/"+day+"/date"
-    fetch(dateUrl)
+    var dateUrl;
+
+    //TODO: Escape control flow hell.
+    if(month >= 1 && month <= 12) {
+        if (month == 2) {
+            if (day >= 1 && day <= 29) {
+                dateUrl = "http://numbersapi.com/"+month+"/"+day+"/date";
+            }
+        }
+        else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            if (day >= 1 && day <= 31) {
+                dateUrl = "http://numbersapi.com/"+month+"/"+day+"/date";
+            }
+        }
+        else {
+            if (day >= 1 && day <= 30) {
+                dateUrl = "http://numbersapi.com/"+month+"/"+day+"/date";
+            }
+        }
+    }
+
+    //TODO: Same as above.
+    if(dateUrl) {
+        fetch(dateUrl)
         .then(res => res.text())
         .then(text => paragraph.innerHTML = text);
+    } else {
+        paragraph.innerHTML = "Enter a valid date.";
+    }
+    
 });
